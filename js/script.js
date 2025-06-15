bc64c549e7ea5c49db306392babb5c4b407d8796
Tomek <tomek.wojak796@gmail.com>
2025-06-15 22:06:28 +0200

    finished navigation

--- js/script.js
+++ js/script.js
@@ -2,25 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
 	// NAV SECTION
 	const navToggler = document.querySelector(".nav__toggler ");
 	const navbar = document.querySelector(".nav__mobile");
-    const allMobileLinks = document.querySelectorAll('.nav__mobile-link')
+	const allMobileLinks = document.querySelectorAll(".nav__mobile-link");
+
+	const openNav = () => {
+		navToggler.classList.add("open");
+		navToggler.setAttribute("aria-expanded", "true");
+		navbar.classList.add("active");
+	};
+	const closeNav = () => {
+		navToggler.classList.remove("open");
+		navToggler.classList.add("close");
+		navToggler.setAttribute("aria-expanded", "false");
+		navbar.classList.remove("active");
+		setTimeout(() => {
+			navToggler.classList.remove("close");
+		}, 400);
+	};
 
 	const handleNav = () => {
 		const isOpened = navToggler.getAttribute("aria-expanded");
 		if (isOpened === "false") {
-			navToggler.classList.add("open");
-			navToggler.setAttribute("aria-expanded", "true");
-			navbar.classList.add("active");
+			openNav();
 		} else {
-			navToggler.classList.remove("open");
-			navToggler.classList.add("close");
-			navToggler.setAttribute("aria-expanded", "false");
-			navbar.classList.remove("active");
-			setTimeout(() => {
-				navToggler.classList.remove("close");
-			}, 400);
+			closeNav();
 		}
 	};
-    
+
+	allMobileLinks.forEach((link) =>
+		link.addEventListener("click", () => {
+			closeNav();
+		})
+	);
 
 	navToggler.addEventListener("click", handleNav);
 });
